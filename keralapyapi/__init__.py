9859ed984f941b5932006df19a3c8f911c57e628
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-31 10:57:49 +0530

    Some updates

---
+++ keralapyapi/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "1.0.2020"
+__license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
+__copyright__ = "Copyright (C) 2020 KeralaPyApi"
+
+from .client import *
+from .client.handlers import *
+from .client.types import *
