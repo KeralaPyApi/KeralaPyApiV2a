b24490aa3593e29cab453d829662fb9cf39e94d9
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-31 11:05:31 +0530

    Update and rename pyrogram/api/core/tl_object.py to keralapyapi/api/core/tl_object.py

--- keralapyapi/api/core/tl_object.py
+++ keralapyapi/api/core/tl_object.py
@@ -1,21 +1,3 @@
-# Pyrogram - Telegram MTProto API Client Library for Python
-# Copyright (C) 2017-2019 Dan Tès <https://github.com/delivrance>
-#
-# This file is part of Pyrogram.
-#
-# Pyrogram is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Pyrogram is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
 from collections import OrderedDict
 from io import BytesIO
 from json import dumps
@@ -53,7 +35,7 @@ class TLObject:
         return dumps(self, indent=4, default=TLObject.default, ensure_ascii=False)
 
     def __repr__(self) -> str:
-        return "pyrogram.api.{}({})".format(
+        return "keralapyapi.api.{}({})".format(
             self.QUALNAME,
             ", ".join(
                 "{}={}".format(attr, repr(getattr(self, attr)))
